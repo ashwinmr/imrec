f3f606815fbe2fc2cfd239d62df1486516c3a82a
ashwinmr <iashwinrao@gmail.com>
2019-11-04 20:33:32 -0800

    add argparse

--- imrec/imrec.py
+++ imrec/imrec.py
@@ -1,11 +1,34 @@
+import argparse
+
 def sqr(n):
   """ Find square of a number
   """
   return n*n
 
+def parse_args():
+  """ Parse arguments for program
+  """
+  parser = argparse.ArgumentParser(description="Program for recommending images")
+
+  subparsers = parser.add_subparsers()
+
+  # Score parser
+  score_parser = subparsers.add_parser('score', help='obtain score of image')
+  score_parser.add_argument('image_path', help='path to image file')
+  score_parser.set_defaults(func=score)
+
+  return parser.parse_args()
+
+def score(args):
+  """ Function to determine score of an image
+  """
+
+  return 10
+
 def main():
 
-  print("Square of 2 is {}".format(sqr(2)))
+  args = parse_args()
+  print(args.func(args))
 
 if __name__ == "__main__":
   main()
