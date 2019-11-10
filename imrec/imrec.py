ad1b3809d81e3eab220503fcc0e5aeb62a03268d
ashwinmr <iashwinrao@gmail.com>
2019-11-09 18:08:26 -0800

    fix subcommand error

--- imrec/imrec.py
+++ imrec/imrec.py
@@ -10,7 +10,7 @@ def parse_args():
   """
   parser = argparse.ArgumentParser(description="Program for recommending images")
 
-  subparsers = parser.add_subparsers()
+  subparsers = parser.add_subparsers(required=True, dest='sub_command')
 
   # Score parser
   score_parser = subparsers.add_parser('score', help='obtain score of image')
